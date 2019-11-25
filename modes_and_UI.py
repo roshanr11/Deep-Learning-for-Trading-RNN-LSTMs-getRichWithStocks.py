61ef78feeae62feab03cffe34ba03608256ee9a0
roshanr11 <roshan.ram0101@gmail.com>
2019-11-25 14:25:18 -0500

    starting UI

--- modes_and_UI.py
+++ modes_and_UI.py
@@ -1,136 +1,131 @@
+# Author: Roshan Ram
+# andrewID: rram
+
 from cmu_112_graphics import *
+# cmu_112_graphics framework credit:
+# http://www.cs.cmu.edu/~112/notes/notes-animations-part2.html
+
+import tkinter as tk
 from tkinter import *
 import random
 from PIL import Image
+# import import_ipynb
+# template: from ipynb.fs.full.<notebookname> import *
+# from ipynb.fs.full.v1mainstockfile_notebookversion import *
+# import v1mainstockfile_notebookversion # ipynb file main
+# from mainstocks import *
+
+import pandas as pd
+from pandas import *
+
+#####
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# source: https://datatofish.com/matplotlib-charts-tkinter-gui/
+#####
 
 
-class SplashScreenMode(Mode):
+class startMode(Mode):
     def redrawAll(mode, canvas):
         font = 'Arial 26 bold'
-        canvas.create_text(mode.width / 2, 150, text='This demos a ModalApp!', font=font)
-        canvas.create_text(mode.width / 2, 200, text='This is a modal splash screen!', font=font)
+        canvas.create_text(mode.width / 2, 150, text='Welcome to', font=font)
+        canvas.create_text(mode.width / 2, 200, text='getRichWithStocks.py', font=font)
         canvas.create_text(mode.width / 2, 250, text='Press any key for the game!', font=font)
 
     def keyPressed(mode, event):
-        mode.app.setActiveMode(mode.app.gameMode)
+        mode.app.setActiveMode(mode.app.instructionsMode)
 
 
-class GameMode(Mode):
+class instructionsMode(Mode):
+    def keyPressed(mode, event):
+        mode.app.setActiveMode(mode.app.mainMode)
+
+    def redrawAll(mode, canvas):
+        font = 'Arial 26 bold'
+        canvas.create_text(mode.width/2, 150, text = 'Instructions: ', font = font)
+        canvas.create_text(mode.width/2, 200, text = 'Click the jawn you wish to jawn! \n\n   Happy stockJawning.', 
+                           font = font)
+
+
+class mainMode(Mode):
     def appStarted(mode):
-        mode.score = 0
-        mode.scrollX = 0
-        mode.player = Player(mode)
-        mode.dots = []
-        mode.dotSet = set()
-        mode.maxX = 3000
-        mode.draggingDot = None
-        numberOfNormalStonks = 0
-        for _ in range(30):
-            newDot = Dot(mode,
-                         random.randint(0, mode.maxX),
-                         random.randint(0, mode.height)
-                         )
-            mode.dots.append(newDot)
+        root = tk.Tk() 
 
     def keyPressed(mode, event):
-        mode.player.move(event.key)
+        pass
 
     # def mouseMoved(mode, event):
     #     mode.cursorPos = [event.x, event.y]
 
     def mousePressed(mode, event):
-        for dot in mode.dots:
-            if dot.containsPoint(event.x + mode.scrollX, event.y):  # there is no mode.scrollY so we good
-                mode.draggingDot = dot
+        pass
 
     def mouseReleased(mode, event):
-        mode.draggingDot = None
+        pass
 
     def mouseDragged(mode, event):
-        if mode.draggingDot is not None:
-            mode.draggingDot.cx = event.x + mode.scrollX
-            mode.draggingDot.cy = event.y
-            mode.player.getDots()
-        # mode.cursorPos = [event.x, event.y]
+        pass
 
-    def redrawAll(mode, canvas):
-        # canvas.create_image(mode.cursorPos[0], cursorPos[1], image=ImageTk.PhotoImage(mode.cursorImage))
-        mode.player.draw(canvas)
-        for stonk in mode.dots: dot.draw(canvas)
-        canvas.create_text(mode.width // 2, 30, text=str(mode.score))
+    # def redrawAll(mode, canvas):
+    #     # root = Tk()
+    #     # figure = plt.Figure(figsize=(6,5), dpi=100)
+    #     # ax = figure.add_subplot(111)
+    #     # chart_type = FigureCanvasTkAgg(figure, root)
+    #     # chart_type.get_tk_widget().pack()
+    #     # df.plot(kind='Chart Type such as bar', legend=True, ax=ax)
+    #     # ax.set_title('The Title of your chart')
+    #     Data1 = {'Country': ['US','CA','GER','UK','FR'],
+    #     'GDP_Per_Capita': [45000,42000,52000,49000,47000]}
 
-
-class Player(object):
-
-    def appStarted(self):
-        self.playerX = self.app.width // 2
-        self.playerY = self.app.height // 2
-        self.scrollX = 0
-        self.scrollMargin = 50
-        self.playerX = self.app.width // 2  # player's center
-        self.dots = []
-        # for i in range(100):
-        #     self.mode.dots.append(Dot(random.randint(0,20)))
-
-        url = 'https://www.trzcacak.rs/myfile/full/441-4411625_parrot-sprite-sheet-bird-transparency.png'
-        spritestrip = self.loadImage(url)  # .transpose(Image.FLIP_LEFT_RIGHT)
-
-        # x, y = spritestrip.size
-        # numSpritesX = 5
-        # numSpritesY = 3
-        # xSpace = x // numSpritesX
-        # ySpace = y // numSpritesY
-        self.sprites = []
-        for j in range(2):
-            for i in range(3):
-                sprite = spritestrip.crop((342 * i, 0 + 300 * j, 342 + 342 * i, 265 + 300 * j))
-                sprite = self.scaleImage(sprite, 1 / 3)
-                self.sprites.append(sprite)
-        self.spriteCounter = 0
-        # self.app.timerDelay = 300
-
-    def makePlayerVisible(self):
-        # scroll to make player visible as needed
-        if (self.playerX < self.scrollX + self.scrollMargin):
-            self.scrollX = self.playerX - self.scrollMargin
-        if (self.playerX > self.scrollX + self.app.width - self.scrollMargin):
-            self.scrollX = self.playerX - self.app.width + self.scrollMargin
-
-    def movePlayer(self, dx, dy):
-        self.playerX += dx
-        self.playerY += dy
-        self.makePlayerVisible()
-        self.playerX -= self.scrollX
-
-    def keyPressed(self, event):
-        if (event.key == "Left"):
-            self.movePlayer(-10, 0)
-        elif (event.key == "Right"):
-            self.movePlayer(+10, 0)
-        elif (event.key == "Up"):
-            self.movePlayer(0, -10)
-        elif (event.key == "Down"):
-            self.movePlayer(0, +10)
-
-    def timerFired(self):
-        self.spriteCounter = (1 + self.spriteCounter) % len(self.sprites)
-
-    def draw(self, canvas):
-        sprite = self.sprites[self.spriteCounter]
-        canvas.create_image(self.playerX, self.playerY, image=ImageTk.PhotoImage(sprite))
-        # canvas.create_image
+    #     df1 = DataFrame(Data1, columns= ['Country', 'GDP_Per_Capita'])
+    #     df1 = df1[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
 
 
-class Dots(object):
-    def __init__(self, mode, x, y):
-        self.x = x
-        self.y = y
-        self.mode = mode
 
-    def draw(self):
-        drawX = self.x - self.mode.scrollX
-        drawY = self.y
-        canvas.create_oval(drawX - 5, drawY - 5, drawX + 5, drawY + 5, fill='yellow')
+    #     Data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
+    #             'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
+    #         }
+        
+    #     df2 = DataFrame(Data2,columns=['Year','Unemployment_Rate'])
+    #     df2 = df2[['Year', 'Unemployment_Rate']].groupby('Year').sum()
+
+
+
+    #     Data3 = {'Interest_Rate': [5,5.5,6,5.5,5.25,6.5,7,8,7.5,8.5],
+    #             'Stock_Index_Price': [1500,1520,1525,1523,1515,1540,1545,1560,1555,1565]
+    #         }
+        
+    #     df3 = DataFrame(Data3,columns=['Interest_Rate','Stock_Index_Price'])
+        
+
+    #     figure1 = plt.Figure(figsize=(6,5), dpi=100)
+    #     ax1 = figure1.add_subplot(111)
+    #     bar1 = FigureCanvasTkAgg(figure1, root)
+    #     bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    #     df1.plot(kind='bar', legend=True, ax=ax1)
+    #     ax1.set_title('Country Vs. GDP Per Capita')
+
+
+    #     figure2 = plt.Figure(figsize=(5,4), dpi=100)
+    #     ax2 = figure2.add_subplot(111)
+    #     line2 = FigureCanvasTkAgg(figure2, root)
+    #     line2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    #     df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
+    #     ax2.set_title('Year Vs. Unemployment Rate')
+
+
+    #     figure3 = plt.Figure(figsize=(5,4), dpi=100)
+    #     ax3 = figure3.add_subplot(111)
+    #     ax3.scatter(df3['Interest_Rate'],df3['Stock_Index_Price'], color = 'g')
+    #     scatter3 = FigureCanvasTkAgg(figure3, root) 
+    #     scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    #     ax3.legend() 
+    #     ax3.set_xlabel('Interest Rate')
+    #     ax3.set_title('Interest Rate Vs. Stock Index Price')
+
+    #     root.mainloop()
 
 
 class HelpMode(Mode):
@@ -143,10 +138,11 @@ class HelpMode(Mode):
 
 class MyModalApp(ModalApp):
     def appStarted(app):
-        app.splashScreenMode = SplashScreenMode()
-        app.gameMode = GameMode()
+        app.startMode = startMode()
+        app.instructionsMode = instructionsMode()
+        app.mainMode = mainMode()
         app.helpMode = HelpMode()
-        app.setActiveMode(app.splashScreenMode)
+        app.setActiveMode(app.startMode)
         app.timerDelay = 50
 
 
